7cbe42f997f45b6d81e5bc6a0421e1de90327050
flavioude <77543535+flavioude@users.noreply.github.com>
2021-05-10 17:43:18 -0300

    1

---
+++ script.js
@@ -0,0 +1,25 @@
+function verificar () {
+   
+   var fano = document.getElementById('txtano')
+   var res = document.getElementById('res')
+   
+
+   if (fano.value.length == 0 || fano.value != '36') {
+       window.alert('Verifique os dados e tente novamente')
+   }  else {
+    window.location.href= "pag02.html"
+
+    
+    
+}
+} 
+
+       
+              
+       
+      
+       
+       
+       
+   
+
